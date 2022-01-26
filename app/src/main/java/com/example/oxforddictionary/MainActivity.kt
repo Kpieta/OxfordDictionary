549870582777ee67bec2582d448732bbacefb362
kpieta <kamil.pieta@capgemini.com>
2022-01-26 18:54:02 +0100

    add hilt DI

--- app/src/main/java/com/example/oxforddictionary/MainActivity.kt
+++ app/src/main/java/com/example/oxforddictionary/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.oxforddictionary
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
---
+++ app/src/main/java/com/example/oxforddictionary/di/AppModule.kt
@@ -0,0 +1,11 @@
+package com.example.oxforddictionary.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@InstallIn(SingletonComponent::class)
+@Module
+class AppModule {
+
+}
