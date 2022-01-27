4bc70fc975bfa5b95aa216e79070681efaaf14ab
kpieta <kamil.pieta@capgemini.com>
2022-01-27 17:47:57 +0100

    move hardcoded ApiCconstants to gradle

--- app/src/main/java/com/example/oxforddictionary/api/ApiConstants.kt
+++ app/src/main/java/com/example/oxforddictionary/api/ApiConstants.kt
@@ -5,6 +5,6 @@ import com.example.oxforddictionary.BuildConfig
 
 object ApiConstants {
     const val BASE_URL = BuildConfig.API_URL
-    const val app_id = "7f925ba2"
-    const val app_key = "ad915499fb6152da7703cba7ccb69e12"
+    const val app_id = BuildConfig.API_ID
+    const val app_key = BuildConfig.API_KEY
 }
